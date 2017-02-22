49d30479b9baf1ce7ff32d9aee818e0f68a5543d
reapercage <h4026@student.jamk.fi>
2017-02-22 14:20:09 +0200

    MVVMDemo Labra13 update; MySQL

---
+++ MVVMDemo/MVVMDemo/Test.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// Interaction logic for Test.xaml
+    /// </summary>
+    public partial class Test : Window
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MVVMDemo.ViewModel.StudentViewModel svmo = new ViewModel.StudentViewModel();
+                svmo.LoadStudentsFromMysql();
+                dgStudents.DataContext = svmo.Students;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
--- MVVMDemo/MVVMDemo/ViewModel/StudentViewModel.cs
+++ MVVMDemo/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace MVVMDemo.ViewModel
 {
@@ -24,6 +25,46 @@ namespace MVVMDemo.ViewModel
             students.Add(new Model.Student { Firstname = "Tomi", LastName = "Töttenström", Asioid = "C1234" });
             students.Add(new Model.Student { Firstname = "Anni", LastName = "Ainokainen", Asioid = "D1234" });
             Students = students; //property
+        }
+        //metodi StudentViewModeliin jolla haetaan oppilastiedot mysql-palvemilta
+        public void LoadStudentsFromMysql()
+        {
+            try
+            {
+                ObservableCollection<Student> students = new ObservableCollection<Student>();
+                //luodaan yhteys labranetin mysql-palvelimelle
+                string connStr = GetMysqlConnectionString();
+                string sql = "SELECT firstname, lastname, asioid FROM student";
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MVVMDemo.Model.Student s = new Model.Student();
+                            s.Firstname = reader.GetString(0);
+                            s.LastName = reader.GetString(1);
+                            s.Asioid = reader.GetString(2);
+                            students.Add(s);
+                        }
+                        Students = students;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private string GetMysqlConnectionString()
+        {
+            string pw = "";
+            //haetaan salasana App.Config- konfiguraatiotiedostosta
+            pw = MVVMDemo.Properties.Settings.Default.passu;
+            //return string.Format("Data source-mysql.labranet.jamk.fi;Initial Catalog-H4026;user-h4026;password-{0}", pw);
+            return string.Format("Data source-mysql.labranet.jamk.fi;Initial Catalog-H4026;user-h4026;");
         }
     }
 }
